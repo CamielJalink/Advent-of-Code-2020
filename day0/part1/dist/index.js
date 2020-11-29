373679347cf83891ca8a97446c895f3b3c19e92a
Camiel <camiel.jalink@gmail.com>
2020-11-29 21:18:36 +0100

    Added eslint to npm start script. This'll probably hurt.

--- day0/part1/dist/index.js
+++ day0/part1/dist/index.js
@@ -1,14 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
-// promisify the readFile node method to read our txt input files.
-// let readInput = readFileSync;
 function advent() {
-    var input = fs_1.readFileSync("input.txt", "utf-8");
+    var stringInput = fs_1.readFileSync("input.txt", "utf-8");
+    var input = stringInput.split("\n");
     console.log(input);
-    // return readInput("input.txt", "utf8").then((input: string) => {
-    //   let wireArray: string[] = input.split("\n");
-    //   findClosestCrossing(wire1, wire2);
-    // })
 }
 advent();
--- day0/part1/src/index.ts
+++ day0/part1/src/index.ts
@@ -3,6 +3,7 @@ import { readFileSync } from "fs";
 function advent() {
   let stringInput: string = readFileSync("input.txt", "utf-8");
   let input: string[] = stringInput.split("\n");
+  console.log(input);
 }
 
 advent();
