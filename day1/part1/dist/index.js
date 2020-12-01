f31595cc2a5797472487475c4ea09860f4c00462
Camiel <camiel.jalink@gmail.com>
2020-12-01 17:23:52 +0100

    Basic day1 implementation. This isn't pretty code just yet

--- day1/part1/dist/index.js
+++ day1/part1/dist/index.js
@@ -2,8 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 function advent() {
-    var stringInput = fs_1.readFileSync("input.txt", "utf-8");
-    var input = stringInput.split("\n");
-    console.log(input);
+    var stringInput = fs_1.readFileSync("input.txt", "utf-8").split("\r\n");
+    var input = stringInput.map(function (stringNum) { return Number(stringNum); });
+    console.log(findPair(input));
+}
+function findPair(input) {
+    var intendedResult = 2020;
+    var correctPair = 0;
+    var correctTriplet = 0;
+    input.forEach(function (num1) {
+        input.forEach(function (num2) {
+            input.forEach(function (num3) {
+                if (num1 + num2 + num3 === intendedResult) {
+                    correctTriplet = num1 * num2 * num3;
+                }
+            });
+            if (num1 + num2 === intendedResult) {
+                correctPair = num1 * num2;
+            }
+        });
+    });
+    return "part one: " + correctPair.toString() + " part two: " + correctTriplet.toString();
 }
 advent();
--- day1/part1/src/index.ts
+++ day1/part1/src/index.ts
@@ -1,9 +1,32 @@
 import { readFileSync } from "fs";
 
 function advent() {
-  let stringInput: string = readFileSync("input.txt", "utf-8");
-  let input: string[] = stringInput.split("\n");
-  console.log(input);
+  const stringInput: string[] = readFileSync("input.txt", "utf-8").split("\r\n");
+  const input: number[] = stringInput.map((stringNum) => Number(stringNum));
+  console.log(findPair(input));
+}
+
+
+function findPair(input: number[]){
+  const intendedResult = 2020;
+  let correctPair: number = 0;
+  let correctTriplet: number = 0;
+
+  input.forEach((num1) => {
+    input.forEach((num2) => {
+      input.forEach((num3) => {
+        if(num1 + num2 + num3 === intendedResult){
+          correctTriplet = num1*num2*num3;
+        }
+      })
+
+      if(num1 + num2 === intendedResult){
+        correctPair = num1*num2;
+      }
+    })
+  })
+
+  return "part one: " + correctPair.toString() + " part two: " + correctTriplet.toString();
 }
 
 advent();
