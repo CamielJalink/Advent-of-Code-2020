b49d895a3e0917b976c579e20fcddbe19e10c04a
Camiel <camiel.jalink@gmail.com>
2020-12-02 13:42:52 +0100

    Part 1 solution added, splitting input in useable data needs some work however

--- day2/part1/dist/index.js
+++ day2/part1/dist/index.js
@@ -3,7 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 function advent() {
     var stringInput = fs_1.readFileSync("input.txt", "utf-8");
-    var input = stringInput.split("\n");
-    console.log(input);
+    var input = stringInput.split("\r\n");
+    var numValidPasswords = 0;
+    input.forEach(function (passwordLine) {
+        if (checkPasswordValid(passwordLine)) {
+            numValidPasswords++;
+        }
+    });
+    console.log(numValidPasswords);
+}
+function checkPasswordValid(passwordLine) {
+    var passLineArray = passwordLine.split(": ");
+    var password = passLineArray[1];
+    var policy = passLineArray[0].split(" ");
+    var policyLetter = policy[1];
+    var policyNumbers = policy[0].split("-");
+    var policyLowerBound = Number(policyNumbers[0]);
+    var policyUpperBound = Number(policyNumbers[1]);
+    // Regular expression gebruiken om te splitten op meerdere zaken? 
+    var policyLetterCount = 0;
+    for (var i = 0; i < password.length; i++) {
+        if (password[i] === policyLetter) {
+            policyLetterCount++;
+        }
+    }
+    if (policyLetterCount >= policyLowerBound && policyLetterCount <= policyUpperBound) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 advent();
--- day2/part1/src/index.ts
+++ day2/part1/src/index.ts
@@ -1,9 +1,42 @@
 import { readFileSync } from "fs";
 
 function advent() {
-  let stringInput: string = readFileSync("input.txt", "utf-8");
-  let input: string[] = stringInput.split("\n");
-  console.log(input);
+  const stringInput: string = readFileSync("input.txt", "utf-8");
+  const input: string[] = stringInput.split("\r\n");
+  
+  let numValidPasswords = 0;
+  input.forEach((passwordLine: string) => {
+    if(checkPasswordValid(passwordLine)){
+      numValidPasswords++;
+    }
+  })
+  console.log(numValidPasswords);
+}
+
+
+function checkPasswordValid(passwordLine: string): boolean{
+  const passLineArray: string[]  = passwordLine.split(": ");
+  const password: string         = passLineArray[1];
+  const policy: string[]         = passLineArray[0].split(" ");
+  const policyLetter: string     = policy[1];
+  const policyNumbers: string[]  = policy[0].split("-");
+  const policyLowerBound: number = Number(policyNumbers[0]);
+  const policyUpperBound: number = Number(policyNumbers[1]);
+
+  // Regular expression gebruiken om te splitten op meerdere zaken? 
+
+  let policyLetterCount = 0;
+  for(let i = 0; i < password.length; i++){
+    if(password[i] === policyLetter){
+      policyLetterCount++;
+    }
+  }
+
+  if(policyLetterCount >= policyLowerBound && policyLetterCount <= policyUpperBound ){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 advent();
