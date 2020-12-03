69aec4b7b85732402aed238a7ba38cb0926fd2af
Camiel <camiel.jalink@gmail.com>
2020-12-03 19:07:59 +0100

    Day3part1 answer

--- day3/part1/dist/index.js
+++ day3/part1/dist/index.js
@@ -3,7 +3,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 function advent() {
     var stringInput = fs_1.readFileSync("input.txt", "utf-8");
-    var input = stringInput.split("\n");
-    console.log(input);
+    var input = stringInput.split("\r\n");
+    console.log(countTrees(input));
+    var test = "aaa";
+    console.log(test[5]);
 }
+function countTrees(slope) {
+    var xStep = 3;
+    var currentXcoord = 0;
+    var treeCount = 0;
+    for (var y = 0; y < slope.length; y++) {
+        var currentYslope = slope[y];
+        while (currentXcoord >= currentYslope.length) {
+            currentYslope += slope[y];
+        }
+        if (currentYslope[currentXcoord] === '#') {
+            console.log("found a tree on line ", y);
+            treeCount++;
+        }
+        currentXcoord += xStep;
+    }
+    return treeCount;
+}
+// Het aantal lijnen naar beneden is de lengte van de array ([0,1,2] = lengte 3, en dat is wat ik wil weten)
+// Ik weet hoeveel characters breed 1 zo'n regel is.
+// Als mijn x-coordinaat hoger is dan die breedte, kopieer dan nog eens die breedte erin, etc. 
+// Ik wil weten of ik op de volgende lijn een beetje bij het juiste cijfertje kan. 
+// Als de x-coordinaat van mijn volgende regel
 advent();
--- day3/part1/src/index.ts
+++ day3/part1/src/index.ts
@@ -3,7 +3,40 @@ import { readFileSync } from "fs";
 function advent() {
   const stringInput: string = readFileSync("input.txt", "utf-8");
   const input: string[] = stringInput.split("\r\n");
-  console.log(input);
+  console.log(countTrees(input));
 }
+
+
+function countTrees(slope: string[]){
+  const xStep: number = 3;
+  let currentXcoord: number = 0;
+  let treeCount: number = 0;
+
+  for(let y = 0; y < slope.length; y++){
+
+    let currentYslope = slope[y];
+    while(currentXcoord >= currentYslope.length){
+      currentYslope += slope[y];
+    }
+
+    if(currentYslope[currentXcoord] === '#'){
+      console.log("found a tree on line ", y);
+      treeCount++;
+    }
+
+    currentXcoord += xStep;
+  }
+
+  return treeCount;
+}
+
+
+// Het aantal lijnen naar beneden is de lengte van de array ([0,1,2] = lengte 3, en dat is wat ik wil weten)
+
+// Ik weet hoeveel characters breed 1 zo'n regel is.
+// Als mijn x-coordinaat hoger is dan die breedte, kopieer dan nog eens die breedte erin, etc. 
+
+// Ik wil weten of ik op de volgende lijn een beetje bij het juiste cijfertje kan. 
+// Als de x-coordinaat van mijn volgende regel
 
 advent();
