6d35ca12a884824c12a3830ac3e90c10ef73077a
Camiel <camiel.jalink@gmail.com>
2020-12-13 15:16:32 +0100

    Day13part1 solution

--- day13/part1/dist/index.js
+++ day13/part1/dist/index.js
@@ -3,7 +3,36 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 function advent() {
     var stringInput = fs_1.readFileSync("input.txt", "utf-8");
-    var input = stringInput.split("\n");
-    console.log(input);
+    var input = stringInput.split("\r\n");
+    var currentTime = Number(input[0]);
+    var stringBusses = input[1].split(",");
+    var busses = [];
+    for (var i = 0; i < stringBusses.length; i++) {
+        if (!isNaN(Number(stringBusses[i]))) {
+            busses.push(Number(stringBusses[i]));
+        }
+    }
+    findEarliestBusDeparture(currentTime, busses);
+}
+// 939
+// 7, 13, 59, 31, 19
+function findEarliestBusDeparture(currentTime, busses) {
+    var nextBusDepartureInMinutes = findEarliestDeparture(currentTime, busses[0]);
+    var nextBus = busses[0];
+    busses.forEach(function (bus) {
+        var busNextDeparture = findEarliestDeparture(currentTime, bus);
+        if (busNextDeparture < nextBusDepartureInMinutes) {
+            nextBusDepartureInMinutes = busNextDeparture;
+            nextBus = bus;
+        }
+    });
+    console.log(nextBus * nextBusDepartureInMinutes);
+}
+function findEarliestDeparture(currentTime, busTravelTime) {
+    var busIteration = 0;
+    while (currentTime > busIteration) {
+        busIteration += busTravelTime;
+    }
+    return busIteration - currentTime;
 }
 advent();
--- day13/part1/src/index.ts
+++ day13/part1/src/index.ts
@@ -3,7 +3,45 @@ import { readFileSync } from "fs";
 function advent() {
   const stringInput: string = readFileSync("input.txt", "utf-8");
   const input: string[] = stringInput.split("\r\n");
-  console.log(input);
+  const currentTime = Number(input[0]);
+  const stringBusses = input[1].split(",");
+
+  let busses: number[] = [];
+  for(let i = 0; i < stringBusses.length; i++){
+    if(!isNaN(Number(stringBusses[i]))) { 
+      busses.push(Number(stringBusses[i]))
+    }
+  }
+  
+  findEarliestBusDeparture(currentTime, busses);
 }
+
+
+
+function findEarliestBusDeparture(currentTime: number, busses: number[]){
+  let nextBusDepartureInMinutes = findEarliestDeparture(currentTime, busses[0]);
+  let nextBus: number = busses[0];
+
+  busses.forEach((bus: number) => {
+    let busNextDeparture = findEarliestDeparture(currentTime, bus);
+    if(busNextDeparture < nextBusDepartureInMinutes){
+      nextBusDepartureInMinutes = busNextDeparture;
+      nextBus = bus;
+    }
+  })
+
+  console.log(nextBus * nextBusDepartureInMinutes);
+}
+
+
+
+function findEarliestDeparture(currentTime: number, busTravelTime: number){
+  let busIteration: number = 0;
+  while(currentTime > busIteration){
+    busIteration += busTravelTime;
+  }
+  return busIteration - currentTime;
+}
+
 
 advent();
